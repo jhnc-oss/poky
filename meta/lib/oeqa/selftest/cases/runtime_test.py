4713591c4e7843b6741c37cca72960b3b5a9b57e
Richard Purdie <richard.purdie@linuxfoundation.org>
2025-05-09 23:12:56 +0100

    fixup
    (From OE-Core rev: df1dca450072f253678d033d8c9c1219c53ba153)
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/selftest/cases/runtime_test.py
+++ meta/lib/oeqa/selftest/cases/runtime_test.py
@@ -166,11 +166,13 @@ TEST_RUNQEMUPARAMS += " slirp"
 
         bitbake('gnupg-native -c addto_recipe_sysroot')
 
+        # gpg requires really short paths
+        homedir = get_bb_var('HOME')
         native_sysroot = get_bb_var("RECIPE_SYSROOT_NATIVE", "gnupg-native")
 
         # Enable package feed signing
-        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-")
-        self.track_for_cleanup(self.gpg_home, dir=os.path.dirname(native_sysroot))
+        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-", dir=homedir)
+        self.track_for_cleanup(self.gpg_home)
         signing_key_dir = os.path.join(self.testlayer_path, 'files', 'signing')
         runCmd('gpgconf --list-dirs --homedir %s; gpg -v --batch --homedir %s --import %s' % (self.gpg_home, self.gpg_home, os.path.join(signing_key_dir, 'key.secret')), native_sysroot=native_sysroot, shell=True)
         features += 'INHERIT += "sign_package_feed"\n'
@@ -204,10 +206,12 @@ TEST_RUNQEMUPARAMS += " slirp"
 
         bitbake('gnupg-native -c addto_recipe_sysroot')
 
+        # gpg requires really short paths
+        homedir = get_bb_var('HOME')
         native_sysroot = get_bb_var("RECIPE_SYSROOT_NATIVE", "gnupg-native")
 
         # Enable package feed signing
-        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-", dir=os.path.dirname(native_sysroot))
+        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-", dir=homedir)
         self.track_for_cleanup(self.gpg_home)
         signing_key_dir = os.path.join(self.testlayer_path, 'files', 'signing')
         runCmd('gpgconf --list-dirs --homedir %s; gpg -v --batch --homedir %s --import %s' % (self.gpg_home, self.gpg_home, os.path.join(signing_key_dir, 'key.secret')), native_sysroot=native_sysroot, shell=True)
